dc413fceff7773f6a1ca749738145bffa083578a
Константин <arkanyga96@gmail.com>
2020-01-16 06:45:03 +0300

    доделанная страница с формой контактов, context для auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,28 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 import './App.css';
 
 const App = () => {
-  return (
-      <Router>
-        <Fragment>
-          <Navbar/>
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component = {Home}/>
-              <Route exact path='/about' component = {About}/>
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-  );
+    return (
+        <AuthState>
+            <ContactState>
+                <Router>
+                    <Fragment>
+                        <Navbar/>
+                        <div className="container">
+                            <Switch>
+                                <Route exact path='/' component={Home}/>
+                                <Route exact path='/about' component={About}/>
+                            </Switch>
+                        </div>
+                    </Fragment>
+                </Router>
+            </ContactState>
+        </AuthState>
+    );
 };
 
 export default App;
---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,34 @@
+import React, {useContext, useRef, useEffect} from 'react';
+import ContactContext from "../../context/contact/contactContext";
+
+function ContactFilter(props) {
+    const contactContext = useContext(ContactContext);
+    const text = useRef("");
+    const {filterContacts, clearFilter, filtered} = contactContext;
+
+    useEffect(()=>{
+        if(!filtered){
+            text.current.value = '';
+        }
+    });
+
+    const onChange = (e) => {
+        if(text.current.value !== ""){
+            filterContacts(e.target.value);
+        } else {
+            clearFilter();
+        }
+    };
+
+
+    return (
+        <form>
+            <input ref={text} type="text"
+                   placeholder="Filter Contacts ..."
+                   onChange={onChange}
+            />
+        </form>
+    );
+}
+
+export default ContactFilter;
